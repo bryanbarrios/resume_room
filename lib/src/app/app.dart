2f791ece41e008c0f8d9ad40b16d947e83e3f1c9
Bryan Barrios <bsbarr13@gmail.com>
2021-08-10 13:33:00 -0600

    chore: add auto route

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:resume_room/src/core/constants/app_constants.dart';
 import 'package:resume_room/src/l10n/l10n.dart';
 import 'package:resume_room/src/core/ui/theme/theme.dart';
 import 'package:resume_room/src/features/landing/landing.dart' show LandingPage;
@@ -23,7 +24,7 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Resume Room',
+      title: kAppName,
       theme: AppTheme.getThemeFromKey(ThemeType.light, context),
       darkTheme: AppTheme.getThemeFromKey(ThemeType.dark, context),
       debugShowCheckedModeBanner: false,
